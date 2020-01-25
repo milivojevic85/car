6c2c175ea64fa341e9455a0be79458618cd8a559
milivojevic85 <misagrf@yahoo.com>
2020-01-25 08:22:46 +0100

    calling properties

--- car.php
+++ car.php
@@ -8,8 +8,22 @@
 <?php 
 class Car
 {
+	public $comp;
+	public $color = "beige";
+	public $hasSunRoof = true;
 	
+	public function hello() {
+		return "Beep I am a <i>".$this->comp."</i>, and I am <i>".$this->color."</i>.<br>";
+	}
 }
+
+$bmw = new Car();
+$mercedes = new Car();
+
+$mercedes->comp = "Mercedes Benz";
+$mercedes->color = "green";
+
+echo $mercedes->hello(); // Beep I am a Mercedes Benz, and I am green.
 
 ?>
 </body>
